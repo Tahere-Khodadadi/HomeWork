c40e76f566751f82cad2f788dc635e5195577573
Tahere Khodadadi <www.taherehkhodadadi1998@gmail.com>
2026-02-17 21:39:16 +0330

    examStatus && studentExam model Layer

---
+++ src/main/java/ir/maktabHW13/model/ExamStatus.java
@@ -0,0 +1,8 @@
+package ir.maktabHW13.model;
+
+public enum ExamStatus {
+    NotStarted,
+    InProgress,
+    Finished,
+    TimeOut
+}
---
+++ src/main/java/ir/maktabHW13/model/StudentExams.java
@@ -0,0 +1,48 @@
+package ir.maktabHW13.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "student_exam")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class StudentExams {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+
+
+    private LocalTime startExamTime;
+    private LocalTime endExamTime=startExamTime;
+    private LocalTime submitDate;
+
+    @Enumerated(EnumType.STRING)
+    private ExamStatus examStatus;
+
+    private int score;
+    private Integer currentQuestionIndex = 0; //  default for first Question
+    private int totalScore = 0;
+
+
+}
+
+
